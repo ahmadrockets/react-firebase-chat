d77e470d3c217044441fea9e35dadfd988349ac8
ahmad.fahrudin <ahmad.fahrudin@logique.co.id>
2021-06-25 22:24:42 +0700

    fixing unused variable

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import React from "react";
 import MainLayout from "./layouts/MainLayout";
-import {useLocation } from "react-router-dom";
 import ChatBox from "./containers/ChatBox";
 import Login from "./containers/Login";
 import firebase from './helpers/Firebase';
